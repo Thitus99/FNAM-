8706ebe340af7751e77fbe5a5f68eb4aa105e032
Thitus99 <71693437+Thitus99@users.noreply.github.com>
2025-08-22 22:16:50 +0200

    finally?!

--- fnam.py
+++ fnam.py
@@ -8,7 +8,7 @@ import winreg
 GITHUB_REPO = "https://github.com/Thitus99/FNAM-.git"
 LOCAL_DIR = os.path.join(os.getenv("APPDATA"), "LIZARD")
 
-ENTRYPOINT = ENTRYPOINT = os.path.join(LOCAL_DIR, "lizard.py")
+ENTRYPOINT = ENTRYPOINT = os.path.join(LOCAL_DIR, "lizard.exe")
 
 def add_to_startup(name, exe_path):
     """Add this exe to Windows autostart via registry."""
